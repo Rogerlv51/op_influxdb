aa0f551af554fec5fbb06bbe65925b902c9e57f0
Rogerlv51 <949608370@qq.com>
2022-08-05 11:42:39 +0800

    delete_data

--- delete_data.py
+++ delete_data.py
@@ -1,6 +1,9 @@
 from influxdb_client import InfluxDBClient
 
-client = InfluxDBClient(url="http://192.168.1.51:32716", token="m9nBYCOJ70_sSn5wDt9EyQfSSWDX4mjAGMt27-d2cF0d_BJsnRML5czj40_IOSW6IS1Uahm5eg0C2Io2QAmENw==")
+url="http://192.168.1.51:32716"
+token="m9nBYCOJ70_sSn5wDt9EyQfSSWDX4mjAGMt27-d2cF0d_BJsnRML5czj40_IOSW6IS1Uahm5eg0C2Io2QAmENw=="
+org="unianalysis"
+client = InfluxDBClient(url=url, token=token, timeout=1000000000)
 
 delete_api = client.delete_api()
 
@@ -9,7 +12,7 @@ Delete Data
 """
 start = "1970-01-01T00:00:00Z"
 stop = "2021-02-01T00:00:00Z"
-delete_api.delete(start, stop, '_measurement="new_uci"', bucket='standard', org='unianalysis')
+delete_api.delete(start, stop, '_measurement="uci_electricity"', bucket='standard', org=org)
 
 """
 Close client
