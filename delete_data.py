f5a67f79f6267f2c3deacb136aa6ea98defa7d05
Rogerlv51 <949608370@qq.com>
2022-08-06 15:43:14 +0800

    done

--- delete_data.py
+++ delete_data.py
@@ -1,6 +1,6 @@
 from influxdb_client import InfluxDBClient
 
-url="http://192.168.1.51:32716"
+url="http://4k377z0213.zicp.vip:53547"
 token="m9nBYCOJ70_sSn5wDt9EyQfSSWDX4mjAGMt27-d2cF0d_BJsnRML5czj40_IOSW6IS1Uahm5eg0C2Io2QAmENw=="
 org="unianalysis"
 client = InfluxDBClient(url=url, token=token, timeout=1000000000)
@@ -11,7 +11,7 @@ delete_api = client.delete_api()
 Delete Data
 """
 start = "1690-01-01T00:00:00Z"
-stop = "2022-08-05T00:00:00Z"
+stop = "2050-08-05T00:00:00Z"
 delete_api.delete(start, stop, '_measurement="tourism_all"', bucket='standard', org=org)
 
 """
--- pro_tourism.py
+++ pro_tourism.py
@@ -1,12 +1,35 @@
-from influxdb_client import InfluxDBClient, Point, Dialect
+
+from influxdb_client import InfluxDBClient, Point, WriteOptions
 bucket = "standard"
 org = "unianalysis"
 token = "m9nBYCOJ70_sSn5wDt9EyQfSSWDX4mjAGMt27-d2cF0d_BJsnRML5czj40_IOSW6IS1Uahm5eg0C2Io2QAmENw=="
 # Store the URL of your InfluxDB instance
-url="http://192.168.1.51:32716"
+url="http://4k377z0213.zicp.vip:53547"
 
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv("tourism.csv")
-print(data[data['landscape']=='m1'])
+data = pd.read_csv("toursim_final.csv")
+df_1 = data.iloc[139624:,:]
+df_1.index = df_1['time']
+print(df_1)
+
+
+
+with InfluxDBClient(url=url, token=token, org=org) as _client:
+
+    with _client.write_api(write_options=WriteOptions(batch_size=1000,
+                                                      flush_interval=10_000,
+                                                      jitter_interval=2_000,
+                                                      retry_interval=5_000,
+                                                      max_retries=5,
+                                                      max_retry_delay=30_000,
+                                                      exponential_base=2)) as _write_client:
+
+
+        """
+        Write Pandas DataFrame
+        """
+
+        _write_client.write("standard", "unianalysis", record=df_1, data_frame_measurement_name='tourism_year',
+                            data_frame_tag_columns=['landscape'])
